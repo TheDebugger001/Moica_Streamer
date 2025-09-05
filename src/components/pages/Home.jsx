ba91de21215b78f8baf0cb00baaa2221c61fcf52
TheDebugger001 <thedebugger001@gmail.com>
2025-09-05 11:53:14 +0300

    Started the Trending movie section

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,24 +4,25 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
+import 'tailwind-scrollbar-hide'
 
-import { Navigation, Pagination, AutoPlay } from 'swiper/modules'
+import { Navigation, Pagination, Autoplay } from 'swiper/modules'
 
 
 const Home = () => {
 
   return (
-    <div className="w-5/6 min-h-screen bg-red-500/10 flex flex-col gap-10">
+    <div className="w-5/6 min-h-screen flex flex-col gap-10">
 
       <div className='11/12 h-[80vh] flex items-center justify-center mt-15'>
-        <Swiper
-        modules={[Navigation, Pagination, AutoPlay]}
-        spaceBetween={20}
+        {/* <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={3}
         slidesPerView={1}
         pagination={{clickable: true}}
         autoplay={{delay: 5000}}
         loop={true}
-        >
+        > */}
         <div className='w-10/11 h-11/12 rounded-xl overflow-hidden'>
           <div className='w-[75.1vw] h-[73.4vh] absolute z-20 rounded-xl flex bg-black/60'>
 
@@ -44,7 +45,34 @@ const Home = () => {
 
             <div className='w-1/2 h-full flex place-items-end justify-center'>
                 <div className='w-10 h-10 bg-fuchsia-500 rounded-full relative z-10 -top-13 left-5'></div>
-              <div className='max-w-9/12 h-1/5 backdrop-blur-2xl bg-black/30 rounded-2xl flex gap-4 overflow-x-auto p-4 scrollbar scroll-smooth relative -top-3'>
+              <div className='max-w-9/12 h-1/5 backdrop-blur-2xl bg-black/30 rounded-2xl flex gap-4 overflow-x-auto p-4 scrollbar-hide scroll-smooth relative -top-3'>
+                <div className='flex-none w-30 h-11/12 rounded-2xl bg-gray-400 overflow-hidden'>
+                  <img src="../images/Star Wars.jpeg" alt="" className='w-full h-full object-cover' />
+                </div>
+                <div className='flex-none w-30 h-11/12 rounded-2xl bg-gray-400 overflow-hidden'>
+                  <img src="../images/Fiercely Dragons.jpeg" alt="" className='w-full h-full object-cover' />
+                </div>
+                <div className='flex-none w-30 h-11/12 rounded-2xl bg-gray-400 overflow-hidden'>
+                  <img src="../images/Avengers Last.jpeg" alt="" className='w-full h-full object-cover' />
+                </div>
+                <div className='flex-none w-30 h-11/12 rounded-2xl bg-gray-400 overflow-hidden'>
+                  <img src="../images/Star Wars.jpeg" alt="" className='w-full h-full object-cover' />
+                </div>
+                <div className='flex-none w-30 h-11/12 rounded-2xl bg-gray-400 overflow-hidden'>
+                  <img src="../images/Fiercely Dragons.jpeg" alt="" className='w-full h-full object-cover' />
+                </div>
+                <div className='flex-none w-30 h-11/12 rounded-2xl bg-gray-400 overflow-hidden'>
+                  <img src="../images/Avengers Last.jpeg" alt="" className='w-full h-full object-cover' />
+                </div>
+                <div className='flex-none w-30 h-11/12 rounded-2xl bg-gray-400 overflow-hidden'>
+                  <img src="../images/Star Wars.jpeg" alt="" className='w-full h-full object-cover' />
+                </div>
+                <div className='flex-none w-30 h-11/12 rounded-2xl bg-gray-400 overflow-hidden'>
+                  <img src="../images/Fiercely Dragons.jpeg" alt="" className='w-full h-full object-cover' />
+                </div>
+                <div className='flex-none w-30 h-11/12 rounded-2xl bg-gray-400 overflow-hidden'>
+                  <img src="../images/Avengers Last.jpeg" alt="" className='w-full h-full object-cover' />
+                </div>
                 <div className='flex-none w-30 h-11/12 rounded-2xl bg-gray-400 overflow-hidden'>
                   <img src="../images/Star Wars.jpeg" alt="" className='w-full h-full object-cover' />
                 </div>
@@ -61,12 +89,33 @@ const Home = () => {
           </div>
           <img src="../images/MJONIR Warrior.jpeg" alt="" className='w-full h-full object-cover' />
         </div>
-        </Swiper>
+        {/* </Swiper> */}
 
       </div>
 
-      <div className='w-10/11 h-100 bg-red-300'>
+      <div className='w-full h-100 flex flex-col items-center justify-between relative -top-10'>
+        <div className='w-11/12'>
+          <p className='font-semibold text-2xl text-fuchsia-500'>Trending Movies</p>
+        </div>
+        <div className='w-11/12 h-10/12 flex'>
+          <div className='w-3/12 h-full rounded-lg hover:bg-fuchsia-500/10 flex flex-col items-center justify-evenly'>
+            <div className='w-12/13 h-10/12 rounded-lg overflow-hidden'>
+              <img src="../images/Uzui Sama.jpeg" alt="" className='w-full h-full object-cover' />
+            </div>
+            <div className='w-12/13 h-1/12'>
+              <p className='font-semibold text-gray-300 text-sm'>Kimetsu no Yaiba: Entertainment District...</p>
+            </div>
+          </div>
 
+          <div className='w-3/12 h-full rounded-lg hover:bg-fuchsia-500/10 flex flex-col items-center justify-evenly'>
+            <div className='w-12/13 h-10/12 rounded-lg overflow-hidden'>
+              <img src="../images/Star Wars.jpeg" alt="" className='w-full h-full object-cover' />
+            </div>
+            <div className='w-12/13 h-1/12'>
+              <p className='font-semibold text-gray-300 text-sm'>Star Wars: The Mixed Worlds</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
